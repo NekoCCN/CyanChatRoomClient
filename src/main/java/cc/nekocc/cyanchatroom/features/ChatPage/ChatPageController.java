03b328eb3f8abab625507285ef63c737d14dae5c
Geonstar <838938323@qq.com>
2025-07-06 15:37:03 +0800

    测试

---
+++ src/main/java/cc/nekocc/cyanchatroom/features/ChatPage/ChatPageController.java
@@ -0,0 +1,17 @@
+package cc.nekocc.cyanchatroom.features.ChatPage;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class ChatPageController {
+    @FXML
+    private AnchorPane main_pane;
+    @FXML
+    private VBox userlist_vbox;
+    @FXML
+    private GridPane chat_pane;
+
+
+}
