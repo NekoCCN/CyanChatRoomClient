f61405443cf7f924401b849be1d5b8f39bc67cad
Ext Culith <unirzine@gmail.com>
2025-07-12 20:45:07 +0800

    ADD UserStatus enum and refactor User class for improved naming consistency.

--- src/main/java/cc/nekocc/cyanchatroom/model/entity/User.java
+++ src/main/java/cc/nekocc/cyanchatroom/model/entity/User.java
@@ -1,19 +1,19 @@
 package cc.nekocc.cyanchatroom.model.entity;
 
-import cc.nekocc.cyanchatroom.model.dto.response.UserLoginResponse;
+import cc.nekocc.cyanchatroom.model.dto.response.*;
 import java.util.UUID;
 
 public class User
 {
     private UUID id_;
-    private String nick_name_;
+    private String nickname_;
     private String avatar_url_;
     private String signature_;
 
-    public User(UserLoginResponse.UserDTO dto)
+    public User(UserOperatorResponse.UserDTO dto)
     {
         this.id_ = dto.id();
-        this.nick_name_ = dto.nick_name();
+        this.nickname_ = dto.nickname();
         this.avatar_url_ = dto.avatar_url();
         this.signature_ = dto.signature();
     }
---
+++ src/main/java/cc/nekocc/cyanchatroom/model/entity/UserStatus.java
@@ -0,0 +1,9 @@
+package cc.nekocc.cyanchatroom.model.entity;
+
+public enum UserStatus
+{
+    ONLINE,
+    BUSY,
+    AWAY,
+    DO_NOT_DISTURB
+}
